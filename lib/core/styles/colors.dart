cd2237a1b625d86fe3db2cf9310995ad9fbb0a14
mondy <mondym8@gmail.com>
2024-04-15 12:48:21 +0200

    Mondy blue

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-  static const Color primary = Color(0xffc21e1e);
+class AppColors {
+  static const Color primary = Color.fromARGB(255, 157, 6, 244);
   static const Color second = Color(0xff000000);
-
-
-
 
   static const Color purple = Color(0xff361E4C);
   static const Color text = Color(0xff8B8B8B);
@@ -14,10 +11,8 @@ class AppColors{
   static const Color offWhite = Color(0xffFBF9F9);
   static const Color malibu = Color(0xff59BCFD);
   static const Color red = Color(0xFFF92809);
-  
+
   static const Color formField = Color(0xFFF8F8F8);
   static const Color formFieldBorder = Color(0xFFE8E8E8);
   static const Color hint = Colors.grey;
-
-
 }
